8d00669eec816cac1d9bb6c9719f04c6c3a359cf
SARPCAN Ercan <ercan.sarpcan@etu.unistra.fr>
2020-12-13 01:40:13 +0100

    Not pure abstract

--- Engine/MalStudio/MalStudio.cpp
+++ Engine/MalStudio/MalStudio.cpp
@@ -9,6 +9,12 @@ MalStudio::MalStudio(float x, float y, std::string title): ChildWindow(x, y, tit
 }
 void MalStudio::Body()  {
     std::cout<<"this my body"<<std::endl;
+    for (int i = 0; i < 10; i++){
+        if (ImGui::Button("Save"))
+        {
+            printf("Pressed button: %d", i);
+        }
+    }
     static float begin = 10, end = 90;
     ImGui::DragFloatRange2("range", &begin, &end, 0.25f, 0.0f, 100.0f, "Min: %.1f %%", "Max: %.1f %%");
 }
